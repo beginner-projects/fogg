aba068a04ac225fabbccd3905132bcc1c7312258
nick <nickj8366@gmail.com>
2024-05-16 22:28:26 +0530

    Update Wallet.tsx

--- src/components/Wallet/Wallet.tsx
+++ src/components/Wallet/Wallet.tsx
@@ -15,25 +15,25 @@ export default function Wallet() {
         <>
             {!wallet.accounts.length && (
                 <div  onClick={connectMetaMask} className={styles.walletcntnr}>
-                    <Image className={styles.walletImage} src="/wallet.svg" alt='wallet-icon' width={30} height={30} />
                     <div className={styles.cwspdtext}>
                         <span className={styles.cwtext}>Connect Wallet</span>
                         <span className={styles.spdtext}>
                             <TokenBalanceFetcher />
                         </span>
                     </div>
+                    <Image className={styles.walletImage} src="/wallet.svg" alt='wallet-icon' width={30} height={30} />
                 </div>
             )}
 
             {wallet.accounts.length > 0 && (
                 <div className={styles.walletcntnr}>
-                    <Image className={styles.walletImage} src="/wallet.svg" alt='wallet-icon' width={30} height={30} />
                     <div className={styles.cwspdtext}>
                         <span className={styles.cwtext}>{formatAddress(wallet.accounts[0])}</span>
                         <span className={styles.spdtext}>
                             <TokenBalanceFetcher />
                         </span>
                     </div>
+                    <Image className={styles.walletImage} src="/wallet.svg" alt='wallet-icon' width={30} height={30} />
                 </div>
             )}
 
